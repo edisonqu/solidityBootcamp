991a54febfe3a25948f7d05afd4abed26f905aff
edison <edisonquu@gmail.com>
2022-10-27 19:36:36 -0400

    test worked for contract A, now implementing volcanoCoin.

---
+++ lets_forge/test/volcanoCoin.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+
+// Standard test libs
+import "forge-std/Test.sol";
+import "forge-std/Vm.sol";
+
+// Contract under test
+import {volcanoCoin} from "../src/volcanoCoin.sol";
+
+contract testingVolcanoCoin is Test{
+
+}
