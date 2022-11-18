fb5e10b04be753cec116094744beafc0f6252943
edison <edisonquu@gmail.com>
2022-11-17 23:34:12 -0500

    started homework20

--- homework18/listenToSpecificFunction.js
+++ homework18/listenToSpecificFunction.js
@@ -1,24 +1,24 @@
 
-const ethers = require("ethers")
+    const ethers = require("ethers")
 
-const provider = new ethers.providers.WebSocketProvider('wss://eth-mainnet.g.alchemy.com/v2/ptDM2tonizEGg_fggxen-ih-S1L4WbTq');
+    const provider = new ethers.providers.WebSocketProvider('wss://eth-mainnet.g.alchemy.com/v2/ptDM2tonizEGg_fggxen-ih-S1L4WbTq');
 
-filter = {
-    address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    topics: [
-        ethers.utils.id("Approval(address,address,uint256)")
-    ]
-}
-provider.on(filter, (log, event) => {
-    // Emitted whenever a DAI token transfer occurs
-    for(i = 0; i<8;i++){
-        console.log("——————————————————————————————————————————————————————————");
-        console.log("——————————————————————————————————————————————————————————");
-        console.log("——————————————————————————————————————————————————————————");
-        console.log("——————————————————————————————————————————————————————————");
-        console.log("——————————————————————————————————————————————————————————");
+    filter = {
+        address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        topics: [
+            ethers.utils.id("Approval(address,address,uint256)")
+        ]
     }
-    console.log("Uniswap");
-    console.log(log);
-    console.log(event);
-})
+    provider.on(filter, (log, event) => {
+        // Emitted whenever a DAI token transfer occurs
+        for(i = 0; i<8;i++){
+            console.log("——————————————————————————————————————————————————————————");
+            console.log("——————————————————————————————————————————————————————————");
+            console.log("——————————————————————————————————————————————————————————");
+            console.log("——————————————————————————————————————————————————————————");
+            console.log("——————————————————————————————————————————————————————————");
+        }
+        console.log("Uniswap");
+        console.log(log);
+        console.log(event);
+    })
